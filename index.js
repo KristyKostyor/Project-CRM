ab3f5a0f0e3d1b53fd75bea3da31176c336512c0
KristyKostyor <ms.kostyor@gmail.com>
2023-04-19 12:56:19 +0300

    add js task

---
+++ index.js
@@ -0,0 +1,8 @@
+let nameProduct = 'Toyta Camry';
+let quantityProduct= 3;
+let categoryProduct= 'Cars';
+let priceProduct = 50000;
+let totalSum = quantityProduct * priceProduct;
+
+console.log(` Наименование товара:${nameProduct}`);
+console.log(`Общая стоимость товара: ${totalSum}$`);
