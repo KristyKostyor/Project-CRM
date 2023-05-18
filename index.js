cdd6cadc0b06e7040f27abfd4b4b60152c54b311
KristyKostyor <ms.kostyor@gmail.com>
2023-05-18 16:18:11 +0300

    назвала константы

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-let nameProduct = 'Toyta Camry';
-let quantityProduct= 3;
-let categoryProduct= 'Cars';
-let priceProduct = 50000;
-let totalSum = quantityProduct * priceProduct;
+const modal = document.querySelector(".modal");
+// Заголовок
+const modalTitle = document.querySelector('.modal__title');
 
-console.log(` Наименование товара:${nameProduct}`);
-console.log(`Общая стоимость товара: ${totalSum}$`);
+// Кнопка возле id и сам id
+const idButton = document.querySelector('.modal__button-cancel');
+const idInput = document.querySelector('#name');
+
+// Форма
+const form = document.querySelector('#form-modal');
+
+// Чекбокс и Поле рядом с чекбоксом
+const checkbox = document.querySelector('#discount');
+const discountSum = document.querySelector('#discount-sum');
+
+// Итоговая стоимость
+const totalPrice = document.querySelector('.modal__price');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+modal.style.display = "flex";
