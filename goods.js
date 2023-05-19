274b45b1518bf48eb794e33bf3f5332831db9442
KristyKostyor <ms.kostyor@gmail.com>
2023-05-19 17:43:01 +0300

    check keys

---
+++ goods.js
@@ -0,0 +1,59 @@
+const goods = [
+  {
+    id: 253842678,
+    title: "Смартфон Xiaomi 11T 8/128GB",
+    category: "mobile-phone",
+    units: "шт",
+    count: 3,
+    price: 27000,
+    images: {
+      small: "img/smrtxiaomi11t-m.jpg",
+      big: "img/smrtxiaomi11t-b.jpg",
+    },
+  },
+  {
+    id: 296378448,
+    title: "Радиоуправляемый автомобиль Cheetan",
+    price: 4000,
+    description:
+      "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
+    category: "toys",
+    discont: 5,
+    count: 1,
+    units: "шт",
+    images: {
+      small: "img/cheetancar-m.jpg",
+      big: "img/cheetancar-b.jpg",
+    },
+  },
+  {
+    id: 215796548,
+    title: "ТВ приставка MECOOL KI",
+    price: 12400,
+    description:
+      "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
+    category: "tv-box",
+    discont: 15,
+    count: 4,
+    units: "шт",
+    images: {
+      small: "img/tvboxmecool-m.jpg",
+      big: "img/tvboxmecool-b.jpg",
+    },
+  },
+  {
+    id: 246258248,
+    title: "Витая пара PROConnect 01-0043-3-25",
+    price: 22,
+    description:
+      "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
+    category: "cables",
+    discont: false,
+    count: 420,
+    units: "v",
+    images: {
+      small: "img/lan_proconnect43-3-25.jpg",
+      big: "img/lan_proconnect43-3-25-b.jpg",
+    },
+  },
+];
--- index.js
+++ index.js
@@ -1,37 +1,53 @@
+'use strict';
+
 const modal = document.querySelector(".modal");
-// Заголовок
-const modalTitle = document.querySelector('.modal__title');
-
-// Кнопка возле id и сам id
-const idButton = document.querySelector('.modal__button-cancel');
-const idInput = document.querySelector('#name');
-
-// Форма
-const form = document.querySelector('#form-modal');
-
-// Чекбокс и Поле рядом с чекбоксом
-const checkbox = document.querySelector('#discount');
-const discountSum = document.querySelector('#discount-sum');
-
-// Итоговая стоимость
-const totalPrice = document.querySelector('.modal__price');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const modalTitle = document.querySelector(".modal__title");
+const closeButton = document.querySelector(".modal__button-cancel");
+const idInput = document.querySelector("#name");
+const form = document.querySelector("#form-modal");
+const checkbox = document.querySelector("#discount");
+const discountSum = document.querySelector("#discount-sum");
+const totalPrice = document.querySelector(".modal__price");
+const overlay = document.querySelector('.overlay');
 
 modal.style.display = "flex";
+
+closeButton.addEventListener("click", function () {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+});
+
+
+const createRow = (obj) => {
+const newRow = document.createElement('tr');
+newRow.className += "table__tr";
+  
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const cell = document.createElement('td');
+      cell.className += "table__cell-td";
+      cell.textContent = obj[key];
+      newRow.appendChild(cell);
+    }
+  } 
+  return newRow;
+};
+const newRow = createRow(goods);
+const tableBody = document.querySelector("tbody");
+
+
+tableBody.appendChild(newRow);
+
+const renderGoods=(goodsArray) =>{
+  const table = document.querySelector("table");
+  tableBody.innerHTML = "";
+
+  const rows = goodsArray.map(createRow);
+
+  rows.forEach(function (row) {
+    table.appendChild(row);
+  });
+}
+
+
+renderGoods(goods);
