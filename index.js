e9db549c5a5119a72960072012614a9e80e12172
KristyKostyor <ms.kostyor@gmail.com>
2023-06-19 19:39:07 +0300

    fixed

--- index.js
+++ index.js
@@ -250,7 +250,15 @@ addNewGood.addEventListener("click", function () {
 
 form.addEventListener("submit", function (event) {
   event.preventDefault(); 
+ const formData = new FormData(event.target);
+ const newRowData = Object.fromEntries(formData.entries());
+ goods.push(newRowData);
 
+ const newRow = createRow(newRowData);
+ tableBody.appendChild(newRow);
+ updateTotalPriceTable();
+
+ form.reset();
   modal.style.display = "none";
   overlay.style.display = "none";
 });
